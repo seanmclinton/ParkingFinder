6b0f8dbac4f92ad0916d4961b54ce9b0cb3b3382
seanmclinton <sean96@vt.edu>
2023-03-11 09:59:32 -0500

    Initialize Spring app and gradle build, first successful build

---
+++ parkingfinder/src/main/java/com/smc/parking/parkingfinder/ParkingfinderApplication.java
@@ -0,0 +1,13 @@
+package com.smc.parking.parkingfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingfinderApplication.class, args);
+	}
+
+}
---
+++ parkingfinder/src/test/java/com/smc/parking/parkingfinder/ParkingfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smc.parking.parkingfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
